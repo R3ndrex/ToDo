bf4573146b046adacb80876101695f7d2436f27d
R3ndrex <yacholohchtoli@gmail.com>
2025-01-10 16:09:10 +0200

    Add common webpack config, use webpack merge library

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,29 +1,6 @@
-const path = require("path");
-const htmlPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
-module.exports = {
-    mode: "development",
-    entry: "./src/script.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpe?g|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-    plugins: [new htmlPlugin({ template: "./src/template.html" })],
-};
+module.exports = merge(common, {
+    mode: "production",
+});
